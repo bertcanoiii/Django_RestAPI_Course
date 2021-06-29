4bac42aee77187240c0628360ce43af62c8f4839
Bert Cano <bertcanoiii@gmail.com>
2021-06-29 09:50:27 -0700

    add http methods to API Views

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from profiles_api import serializers
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API view!"""
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, response, format=None):
+        """Returns list of API features"""
+        an_apiview = [
+            "Uses HTTP Methods as functions",
+            "Similar to traditional Django views",
+            "Give the most control over your application logic",
+            "Is mapped manually to urls"
+        ]
+
+        return Response({"message": "Hello!", "an_apiview": an_apiview})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get("name")
+            message = f"Hello {name}!"
+            return Response({"message": message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+        return Response({"method": "PUT"})
+
+    def patch(self, request, pk=None):
+        """Handle a partial update of an object"""
+        return Response({"method": "PATCH"})
+
+    def delete(self, request, pk=None):
+        """Delete and object"""
+        return Response({"method": "DELETE"})
